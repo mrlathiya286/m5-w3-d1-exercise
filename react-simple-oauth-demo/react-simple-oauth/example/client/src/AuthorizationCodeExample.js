542241436f81af4a93c01d6d568e973d5445360f
Maulik <167531191+mrlathiya286@users.noreply.github.com>
2024-09-17 20:07:29 -0600

    week 3 day 1 exercise

--- react-simple-oauth-demo/react-simple-oauth/example/client/src/AuthorizationCodeExample.js
+++ react-simple-oauth-demo/react-simple-oauth/example/client/src/AuthorizationCodeExample.js
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
-import OAuth2Login from "../../../src/OAuth2Login";
-import ErrorAlert from "./ErrorAlert";
+import React, { useState } from 'react';
+import OAuth2Login from '../../../src/OAuth2Login';
+import ErrorAlert from './ErrorAlert';
 import {
   authorizationUrl,
   clientId,
   redirectUri,
-  serverUrl,
-} from "./settings-code";
+  serverUrl
+} from './settings-code';
 
 export default function AuthorizationCodeExample() {
   const [accessToken, setAccessToken] = useState(null);
   const [error, setError] = useState(null);
 
   // You can test this with a GitHub OAuth2 app (provided test server supports GitHub and Spotify)
-  const onSuccess = ({ code }) =>
-    fetch(`${serverUrl}/github/token?code=${code}`)
-      .then((res) => res.json())
-      .then((data) => setAccessToken(data.access_token));
+  const onSuccess = ({ code }) => fetch(`${serverUrl}/github/token?code=${code}`)
+    .then(res => res.json())
+    .then(data => setAccessToken(data.access_token));
 
   return (
     <div className="column">
-      {error && <ErrorAlert error={error} />}
+      {
+        error && <ErrorAlert error={error} />
+      }
       <OAuth2Login
         authorizationUrl={authorizationUrl}
         clientId={clientId}
@@ -30,7 +31,9 @@ export default function AuthorizationCodeExample() {
         onSuccess={onSuccess}
         onFailure={setError}
       />
-      {accessToken && <p>Access token: {accessToken}</p>}
+      {
+        accessToken && <p>Access token: {accessToken}</p>
+      }
     </div>
   );
 }
--- react-simple-oauth-demo/react-simple-oauth/example/client/src/index.js
+++ react-simple-oauth-demo/react-simple-oauth/example/client/src/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("example"));
+ReactDOM.render(
+  <App />,
+  document.getElementById('example'),
+);
